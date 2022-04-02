4ec933fa2f5840c61bd0b56318946a69bc561f6a
jayme <jaymeliao1226@gmail.com>
2022-04-01 22:15:33 -0400

    init commit

--- EyesHelper/ViewController.swift
+++ EyesHelper/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var loadTextIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        stopAnimate()
     }
 
+    private func startAnimate(){
+        self.loadTextIndicator.startAnimating()
+    }
+    
+    
+    
+    private func stopAnimate(){
+        self.loadTextIndicator.stopAnimating()
+    }
+    
+    @IBAction func takePicture(_ sender: UIButton) {
+        setupGallery();
+    }
+    
+    
+    private func setupGallery() {
+        //todo: to be change to camera
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            
+            let imagePhotoLibaryPicker = UIImagePickerController()
+            imagePhotoLibaryPicker.delegate = self
+            imagePhotoLibaryPicker.allowsEditing=true
+            imagePhotoLibaryPicker.sourceType = .photoLibrary
+            self.present(imagePhotoLibaryPicker, animated:true, completion: nil)
+            
+        }
+    }
+}
 
+
+
+extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let image=info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        //print(image!)
+        self.photoImage.image=image
+    }
+    
+    
 }
 
